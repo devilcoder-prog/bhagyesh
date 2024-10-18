6699f484d93bf9e029c76290dcf6f8354d73235e
BHAGYESH MALI <bhagyeshmali535@gmail.com>
2024-10-18 11:16:05 +0530

    added new one

---
+++ 2darray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    int i,j;
+    int rows,cols;
+    int arr[rows][cols];
+    cout<<"Enter rows and column";
+    cin>>rows>>cols;
+    for(i=0;i<rows;i++)
+    {
+      for(j=0;j<cols;j++)
+      {
+        cin>>arr[i][j];
+        cout<<endl;
+      }
+    }
+    for(i=0;i<rows;i++)
+    {
+      for(j=0;j<cols;j++)
+      {
+        cout<<arr[i][j];
+        cout<<endl;
+      }
+    }
+
+    
+}
